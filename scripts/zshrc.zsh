0a3f7870bff78dd2dcda645e72ce6f1922d75659
xcdnlgd <xcdnlgd@hotmail.com>
2024-12-03 23:48:32 +0800

    remove rga-fzf() from zshrc.zsh, now ripgrep-all builtin

--- scripts/zshrc.zsh
+++ scripts/zshrc.zsh
@@ -97,19 +97,5 @@ source /usr/share/fzf/key-bindings.zsh
 source /usr/share/fzf/completion.zsh
 # zvm_after_init_commands+=('source /usr/share/fzf/key-bindings.zsh && source /usr/share/fzf/completion.zsh')
 
-function rga-fzf() {
-	RG_PREFIX="rga --files-with-matches"
-	local file
-	file="$(
-		FZF_DEFAULT_COMMAND="$RG_PREFIX '$1'" \
-			fzf --sort --preview="[[ ! -z {} ]] && rga --pretty --context 5 {q} {}" \
-				--phony -q "$1" \
-				--bind "change:reload:$RG_PREFIX {q}" \
-				--preview-window="70%:wrap"
-	)" &&
-	echo "opening $file" &&
-	xdg-open "$file"
-}
-
 eval "$(starship init zsh)"
 eval "$(zoxide init zsh)"
