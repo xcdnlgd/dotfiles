d1ec91c325afe1373728a6fb348bd10aa4186eab
xcdnlgd <xcdnlgd@hotmail.com>
2024-12-09 12:28:54 +0800

    update tmux config, about mouse
    double click, drag, now don't copy and quit copy-mode. click in
    copy-mode now move cursor

--- bootstrap.sh
+++ bootstrap.sh
@@ -4,6 +4,7 @@ set -x
 mkdir -p $HOME/.config
 mkdir -p $HOME/scripts
 
+git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
 ln -s $PWD/.tmux.conf $HOME
 ln -s $PWD/.config/starship.toml $HOME/.config
 echo 'source ~/scripts/zshrc.zsh' > $HOME/.zshrc
