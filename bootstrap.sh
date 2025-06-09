cd54a309c03ed04110de25bc17c382832da9eafe
xcdnlgd <xcdnlgd@hotmail.com>
2025-06-09 17:45:00 +0800

    bootstrap.sh less manual

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,39 +1,43 @@
 #!/bin/bash
 set -x
 
-mkdir -p $HOME/.config
-mkdir -p $HOME/scripts
+create_symlink() {
+    local src="$1"
+    local dest="$2"
+
+    mkdir -p "$(dirname "$dest")"
+    ln -s "$src" "$dest"
+}
+
+scan_and_link() {
+    local source_dir="$1"
+    local target_dir="$2"
+
+    for item in "$source_dir"/*; do
+        if [ -e "$item" ]; then
+            local base_name=$(basename "$item")
+            local new_source="$source_dir/$base_name"
+            local new_target="$target_dir/$base_name"
+
+            if [ -d "$new_source" ]; then
+                scan_and_link "$new_source" "$new_target"
+            elif [ -f "$new_source" ]; then
+                create_symlink "$new_source" "$new_target"
+            fi
+        fi
+    done
+}
+
+scan_and_link "$PWD/.config" "$HOME/.config"
+scan_and_link "$PWD/.local" "$HOME/.local"
 
 ln -s $PWD/.tmux.conf $HOME
+ln -s $PWD/.clang-format $HOME
 
-ln -s $PWD/.config/starship.toml $HOME/.config
-
+mkdir -p $HOME/scripts
 echo 'source ~/scripts/zshrc.zsh' > $HOME/.zshrc
 ln -s $PWD/scripts/zshrc.zsh $HOME/scripts
-
-mkdir -p $HOME/.config/yazi
-ln -s $PWD/.config/yazi/keymap.toml $HOME/.config/yazi
-
-mkdir -p $HOME/.config/wireplumber/wireplumber.conf.d
-ln -s $PWD/.config/wireplumber/wireplumber.conf.d/51-disable-suspension.conf $HOME/.config/wireplumber/wireplumber.conf.d
-
-mkdir -p $HOME/.config/fish
-ln -s $PWD/.config/fish/config.fish $HOME/.config/fish
-
-mkdir -p $HOME/.local/share/fcitx5/rime
-ln -s $PWD/.local/share/fcitx5/rime/default.custom.yaml $HOME/.local/share/fcitx5/rime
-mkdir -p $HOME/.config/environment.d
-ln -s $PWD/.config/environment.d/im.conf $HOME/.config/environment.d
-
-mkdir -p $HOME/.config/fontconfig/conf.d/
-ln -s $PWD/.config/fontconfig/conf.d/64-language-selector-prefer.conf $HOME/.config/fontconfig/conf.d/
 
 mkdir -p $HOME/.cargo
 ln -s $PWD/.cargo/config.toml $HOME/.cargo
 
-mkdir -p $HOME/.config/mpv
-ln -s $PWD/.config/mpv/mpv.conf $HOME/.config/mpv
-mkdir -p $HOME/.config/mpv/scrips
-ln -s $PWD/.config/mpv/scripts/sub-fonts-dir-auto.lua $HOME/.config/mpv/scrips
-
-ln -s $PWD/.clang-format $HOME
